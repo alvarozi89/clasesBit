e6385eba5fc906dda9cd9cae0b43dac2af41339a
alvarozi89 <alvarozi@hotmail.es>
2022-05-03 21:52:33 -0500

    clase 3 mayo

--- Novena semana/clase 2/clase2/src/app/app.module.ts
+++ Novena semana/clase 2/clase2/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Prueba1Component } from './componentes/prueba1/prueba1.component';
+import { Prueba2Component } from './componentes/prueba2/prueba2.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Prueba1Component
+    Prueba1Component,
+    Prueba2Component
   ],
   imports: [
     BrowserModule,
---
+++ Novena semana/clase 2/clase2/src/app/componentes/prueba2/prueba2.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba2',
+  templateUrl: './prueba2.component.html',
+  styleUrls: ['./prueba2.component.css']
+})
+export class Prueba2Component implements OnInit {
+  public titulo ="Componente 2";
+  public nombrePrueba= "alvaro"
+  public nombrePrueba2= "ALVARO"
+
+  public consolas:any
+
+  // public consolas =
+  // [
+  //   {
+  //   marca:"Xbox",
+  //   anio: "2006",
+  //   precio: 500
+  //   },
+
+  //   {
+  //   marca:"ps5",
+  //   anio: "2004",
+  //   precio: 600
+  //   },
+  //   {
+  //   marca:"nintendo",
+  //   anio: "2001",
+  //   precio: 300
+  //   }
+
+  // ];
+
+  public anioLanzamiento = new Date();
+  public precio = 200;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+    this.carga()
+    this.listarTodasPersonas();
+
+    //validar roles, permisos, token
+
+
+  }
+
+  carga(){
+      setTimeout(() => {
+        this.consolas =
+        [
+          {
+          marca:"Xbox",
+          anio: "2006",
+          precio: 500
+          },
+
+          {
+          marca:"ps5",
+          anio: "2004",
+          precio: 600
+          },
+          {
+          marca:"nintendo",
+          anio: "2001",
+          precio: 300
+          }
+        ];
+    }, 1000);
+  }
+
+
+  //esta funciones deben centrarse en envetos, como clic, change
+  eliminarPersona(){
+
+  }
+
+
+  actualizarPersona(){
+
+  }
+
+  listarTodasPersonas(){
+
+  }
+
+
+}
